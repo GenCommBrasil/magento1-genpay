1da4458b85779b3a42c3ba9fbcb75f9f85bbba0a
Alex <alexsilva.info@gmail.com>
2019-01-30 11:33:15 -0200

    ajustado o JS para gerar token e fingerprint de baixo nivel

--- skin/frontend/base/default/rakuten/rakutenpay/js/boleto.js
+++ skin/frontend/base/default/rakuten/rakutenpay/js/boleto.js
@@ -16,41 +16,27 @@
  ************************************************************************
  */
 function validateBoletoForm(save) {
-    //Clears the div and adds the required RakutenPay method to the form
-    var container = document.getElementById("rakutenpay-billet-method-div");
-    while (container.hasChildNodes()) {
-        container.removeChild(container.lastChild);
-    }
-    var rpay_method = document.createElement("input");
-    rpay_method.type = "hidden";
-    rpay_method.setAttribute("data-rkp", "method");
-    rpay_method.value = "billet";
-    container.appendChild(rpay_method);
 
-    //Gets the form element
-    var form = rpay_method.form;
-
-    //Generates the fingerprint and token
+    console.log("call generateFingerprint");
     var rpay = new RPay();
-    rpay.listeners = {
-        "result:success": function(){
-            //Hides the fingerprint and token fields (don't know why it isn't by default)
-            document.getElementsByName('rkp[fingerprint]')[0].type = "hidden";
-            document.getElementsByName('rkp[fingerprint]')[0].id = "payment[fingerprint]";
-            document.getElementsByName('rkp[fingerprint]')[0].name = "payment[fingerprint]";
-            save();
-        },
-        "result:error":   function(errors){
-            console.log(errors);
+    var fingerprintFields = document.querySelectorAll(".rakutenFingerprint");
+    rpay.fingerprint(function(error, fingerprint) {
+        if (error) {
+            console.log("Erro ao gerar fingerprint", error);
+            return;
         }
-    };
-    rpay.generate(form);
+        console.log("complete generateFingerprint");
+        for (var i = 0; i < fingerprintFields.length; i++) {
+            fingerprintFields[i].value = fingerprint;
+        }
+    });
 
-  if (validateDocument(document.querySelector('#bilitDocument'))) {
-    return true;
+    if (validateDocument(document.querySelector('#bilitDocument'))) {
+        save();
+        return true;
   }
 
-  return false;
+    return false;
 }
 
 function validateBoletoFormOneStepCheckout() {
