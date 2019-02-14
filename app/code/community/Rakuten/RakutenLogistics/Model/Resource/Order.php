d09fc61d413c25832a9ed05d27755c34642e5db2
Alex <alexsilva.info@gmail.com>
2019-02-14 12:33:22 -0200

    adicionado license nos arquivos

--- app/code/community/Rakuten/RakutenLogistics/Model/Resource/Order.php
+++ app/code/community/Rakuten/RakutenLogistics/Model/Resource/Order.php
@@ -1,11 +1,25 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: alex.silva
- * Date: 2019-02-13
- * Time: 17:34
+ ************************************************************************
+ * Copyright [2018] [RakutenConnector]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ************************************************************************
  */
 
+/**
+ * Class Rakuten_RakutenLogistics_Model_Resource_Order
+ */
 class Rakuten_RakutenLogistics_Model_Resource_Order extends Mage_Core_Model_Resource_Db_Abstract
 {
     /***
